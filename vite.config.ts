f5d18da2d8b4f78e6fedca3f9720063ab95a7d86
wanghaisheng <edwin_uestc@163.com>
2025-01-03 13:50:32 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -9,18 +9,4 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: '/index.html'
-      },
-      output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom'],
-        },
-      },
-    },
-  },
 })
