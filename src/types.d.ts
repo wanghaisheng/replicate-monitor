b803e84e67ceeda95929f052a51d31a0c98854e8
wanghaisheng <edwin_uestc@163.com>
2025-01-03 14:01:57 +0800

    fix

---
+++ src/types.d.ts
@@ -0,0 +1,35 @@
+/// <reference types="@cloudflare/workers-types" />
+
+interface Env {
+  DB: D1Database;
+}
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.png" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.gif" {
+  const content: string;
+  export default content;
+}
+
+declare module "*.webp" {
+  const content: string;
+  export default content;
+}
